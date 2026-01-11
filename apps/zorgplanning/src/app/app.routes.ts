1d417deb94b05a290a02872e0945b26562f4b6a3
davidsennekool <dsennekool@gmail.com>
2026-01-11 17:24:48 +0100

    feature: initialized nx monorepo and base angular application

---
+++ apps/zorgplanning/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Route } from '@angular/router';
+import { FeatureDashboard } from '@zorgplanning/feature-dashboard';
+import { FeatureClientDetail } from '@zorgplanning/feature-client-detail';
+
+export const appRoutes: Route[] = [];
